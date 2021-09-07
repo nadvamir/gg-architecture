c6c58e8e89095430c01287d53b2f98c51d2e9cd3
N/A <N/A>
2021-09-07 10:12:21 +0100

    Navigate as an event callback

--- souls-adrift/src/game-client/pages/LoginScreen.jsx
+++ souls-adrift/src/game-client/pages/LoginScreen.jsx
@@ -1,11 +1,25 @@
-import { Link } from "solid-app-router";
+import { Link, useNavigate } from "solid-app-router";
+
+import appStyles from "../../App.module.css";
 
 function LoginScreen() {
+  const navigate = useNavigate()
+  function signIn() {
+    navigate("/loc")
+  }
+
   return (
-    <div>
-        <p>Login Screen</p>
-        <Link href="/loc">Sign In</Link><br/>
-        <Link href="/reg">Register</Link>
+    <div id="login-screen" class={appStyles['main-screen']}>
+      <header>
+        <h1>Souls Adrift</h1>
+        <p>A text-based adventure.</p>
+      </header>
+      <section class={appStyles['large-form']}>
+        <label for="username">Username</label>
+        <input type="text" id="username" name="username" />
+        <button id="login-button" onClick={signIn}>Log In</button>
+      </section>
+      <Link href="/reg">Register</Link>
     </div>
   );
 }
