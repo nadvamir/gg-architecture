d33df9449c3fa83cdf7cfbde70c59c2b644cf35a
Maksim Solovjov <himaxx2@gmail.com>
2021-10-02 08:57:49 +0100

    Allow refresh on the character screen

--- souls-adrift/src/game-client/pages/CharacterScreen.jsx
+++ souls-adrift/src/game-client/pages/CharacterScreen.jsx
@@ -33,53 +33,65 @@ function EquippedSection(props) {
   )
 }
 
-function CharacterScreen() {
+function CharacterScreenImpl() {
   const state = gameEngine.getState()
   const player = gameEngine.get(state.uid)
 
   return (
-    <div id={styles['character-screen']} class={[styles['main-screen'], styles['page']].join(' ')}>
-      <div class={styles['content']}>
-        <header>
-          <StatusBar />
-          <h1>[1] __Blind_Augur__</h1>
-          <div>
-            Chapter 1: Soul Stranded
-          </div>
-          <div class={styles['location-info']}>
-            <p>I was told that no ships will visit the harbour until spring. I can't leave, and I'm running out of money.</p>
-          </div>
-        </header>
-        <div class={styles['divider']}>
-          Character
-        </div>
+    <div class={styles['content']}>
+      <header>
+        <StatusBar />
+        <h1>[1] __Blind_Augur__</h1>
         <div>
-          <div class={styles['item']}>Experiece: {player.exp()}/{player.maxExp()}</div>
-          <div class={styles['item']}>Skill points: {player.skillPoints()}</div>
-          <div class={styles['item']}>Damage: {player.minDmg()}–{player.maxDmg()}</div>
-          <div class={styles['item']}>Armour: {player.armour()}</div>
-          <div class={styles['item']}>Attack points: {player.attackPoints()}</div>
+          Chapter 1: Soul Stranded
+          </div>
+        <div class={styles['location-info']}>
+          <p>I was told that no ships will visit the harbour until spring. I can't leave, and I'm running out of money.</p>
         </div>
-        <div class={styles['divider']}>
-          Skills
+      </header>
+      <div class={styles['divider']}>
+        Character
         </div>
-        <SkillList skills={player.skills()} />
-        <EquippedSection player={player} />
-        <div class={styles['divider']}>
-          Items
-        </div>
-        <div>
-          {player.inventory().map(([item, count]) => {
-            return <div class={styles['item']}>
-              <InfoModalLink actor={item} />
-              <ItemCountIndicator count={count} />
-              <EquipItemLink item={item} skills={player.skills()} />
-              <ConsumeItemLink item={item} />
-              <DropItemLink item={item} count={count} />
-            </div>
-          })}
-        </div>
+      <div>
+        <div class={styles['item']}>Experiece: {player.exp()}/{player.maxExp()}</div>
+        <div class={styles['item']}>Skill points: {player.skillPoints()}</div>
+        <div class={styles['item']}>Damage: {player.minDmg()}–{player.maxDmg()}</div>
+        <div class={styles['item']}>Armour: {player.armour()}</div>
+        <div class={styles['item']}>Attack points: {player.attackPoints()}</div>
       </div>
+      <div class={styles['divider']}>
+        Skills
+        </div>
+      <SkillList skills={player.skills()} />
+      <EquippedSection player={player} />
+      <div class={styles['divider']}>
+        Items
+        </div>
+      <div>
+        {player.inventory().map(([item, count]) => {
+          return <div class={styles['item']}>
+            <InfoModalLink actor={item} />
+            <ItemCountIndicator count={count} />
+            <EquipItemLink item={item} skills={player.skills()} />
+            <ConsumeItemLink item={item} />
+            <DropItemLink item={item} count={count} />
+          </div>
+        })}
+      </div>
+    </div>
+  );
+}
+
+function CharacterScreen() {
+  const state = gameEngine.getState()
+
+  return (
+    <div id={styles['character-screen']} class={[styles['main-screen'], styles['page']].join(' ')}>
+      <Show when={!state.uid} fallback={<CharacterScreenImpl />}>
+        <div class={state.uid ? styles['hidden'] : styles['game-loading']}>
+          <h1>Loading...</h1>
+        </div>
+      </Show>
     </div>
   );
 }
--- souls-adrift/src/game-client/pages/NpcScreen.jsx
+++ souls-adrift/src/game-client/pages/NpcScreen.jsx
@@ -2,6 +2,8 @@ import { Link, useData } from "solid-app-router";
 
 import styles from "../../App.module.css";
 
+import { gameEngine } from "../../game-engine/GameAssembly";
+
 import { StatusBar } from "../items/StatusBar.jsx"
 import { InfoModalLink } from "../items/InfoModalLink.jsx"
 import {
@@ -17,19 +19,19 @@ function ItemsForSale(props) {
   const items = npc.forSale()
   return (
     <Show when={items.length > 0}>
-        <div class={styles['divider']}>
-          Buy (you have £{player.moneyCount()})
+      <div class={styles['divider']}>
+        Buy (you have £{player.moneyCount()})
         </div>
-        <div>
-          {items.map(([item, count]) => {
-            return (<div class={styles['item']}>
-              <InfoModalLink actor={item} />
-              <ItemCountIndicator count={count} />
-              <BuyItemLink item={item} player={player} cost={npc.bidPrice(item, player)} count={count} npc={npc} />
-              <ItemCostIndicator cost={npc.bidPrice(item, player)} />
-              </div>)
-          })}
-        </div>
+      <div>
+        {items.map(([item, count]) => {
+          return (<div class={styles['item']}>
+            <InfoModalLink actor={item} />
+            <ItemCountIndicator count={count} />
+            <BuyItemLink item={item} player={player} cost={npc.bidPrice(item, player)} count={count} npc={npc} />
+            <ItemCostIndicator cost={npc.bidPrice(item, player)} />
+          </div>)
+        })}
+      </div>
     </Show>
   )
 }
@@ -40,49 +42,62 @@ function ItemsToSell(props) {
   const items = npc.willBuy(player.inventory())
   return (
     <Show when={items.length > 0}>
-        <div class={styles['divider']}>
-          Sell (trader has £{npc.moneyCount()})
+      <div class={styles['divider']}>
+        Sell (trader has £{npc.moneyCount()})
         </div>
-        <div>
-          {items.map(([item, count]) => {
-            return (<div class={styles['item']}>
-              <InfoModalLink actor={item} />
-              <ItemCountIndicator count={count} />
-              <SellItemLink item={item} npc={npc} cost={npc.askPrice(item, player)} count={count} npc={npc} />
-              <ItemCostIndicator cost={npc.askPrice(item, player)} />
-              </div>)
-          })}
-        </div>
+      <div>
+        {items.map(([item, count]) => {
+          return (<div class={styles['item']}>
+            <InfoModalLink actor={item} />
+            <ItemCountIndicator count={count} />
+            <SellItemLink item={item} npc={npc} cost={npc.askPrice(item, player)} count={count} npc={npc} />
+            <ItemCostIndicator cost={npc.askPrice(item, player)} />
+          </div>)
+        })}
+      </div>
     </Show>
   )
 }
 
-function NpcScreen() {
-  const {npc, player} = useData()()
+function NpcScreenImpl() {
+  const { npc, player } = useData()()
 
   return (
-    <div id={styles['npc-screen']} class={[styles['main-screen'], styles['page']].join(' ')}>
-      <div class={styles['content']}>
-        <header>
-          <StatusBar/>
-          <h1>{npc.name()}</h1>
-          <div class={styles['location-info']}>
-            Hey, what brings you here?
+    <div class={styles['content']}>
+      <header>
+        <StatusBar />
+        <h1>{npc.name()}</h1>
+        <div class={styles['location-info']}>
+          Hey, what brings you here?
           </div>
-        </header>
-        <div class={styles['divider']}>
-          Dialog
+      </header>
+      <div class={styles['divider']}>
+        Dialog
         </div>
-        <div>
-          <div class={styles['item']}><Link href="/inspect/:sailor">I'm looking for work</Link></div>
-          <div class={styles['item']}><Link href="/inspect/:sailor">I believe you lost something</Link> (give Amulet)</div>
-          <div class={styles['item']}><Link href="/location">Just passing by</Link> (end)</div>
-        </div>
-        <ItemsForSale npc={npc} player={player} />
-        <ItemsToSell npc={npc} player={player} />
+      <div>
+        <div class={styles['item']}><Link href="/inspect/:sailor">I'm looking for work</Link></div>
+        <div class={styles['item']}><Link href="/inspect/:sailor">I believe you lost something</Link> (give Amulet)</div>
+        <div class={styles['item']}><Link href="/location">Just passing by</Link> (end)</div>
       </div>
+      <ItemsForSale npc={npc} player={player} />
+      <ItemsToSell npc={npc} player={player} />
     </div>
   );
 }
+
+function NpcScreen() {
+  const state = gameEngine.getState()
+
+  return (
+    <div id={styles['npc-screen']} class={[styles['main-screen'], styles['page']].join(' ')}>
+      <Show when={!state.uid} fallback={<NpcScreenImpl />}>
+        <div class={state.uid ? styles['hidden'] : styles['game-loading']}>
+          <h1>Loading...</h1>
+        </div>
+      </Show>
+    </div>
+  );
+}
+
 
 export default NpcScreen;
